9f6551a71f62487dde45934d245b49884897700e
auxout <tkc@auxout.jp>
2011-08-15 18:28:33 +0900

    logger 消し忘れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   
   def self.find_for_twitter_auth_oauth(access_token, signed_in_resource=nil)
       data = access_token['extra']['user_hash']
-      logger.info(access_token)
+
       if user = User.find_by_uid(access_token['uid'].to_i)
         user
       else # Create a user with a stub password. 
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
     
     def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
       data = access_token['extra']['user_hash']
-      logger.info(access_token)
+
       if user = User.find_by_uid(access_token['uid'].to_i)
         user
       else # Create a user with a stub password. 
